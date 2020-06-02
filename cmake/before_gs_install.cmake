f410d55530ce32c0eb8ac4067fdcf215e7fb4b69
ayase <137056643@qq.com>
2020-06-02 22:51:17 +0800

    sol2

--- cmake/before_gs_install.cmake
+++ cmake/before_gs_install.cmake
@@ -2,12 +2,16 @@ file(GLOB_RECURSE method_headers ${CMAKE_MOD_ELUNA_ENGINE_DIR}/LuaEngine/*Method
 file(GLOB_RECURSE sources_ElunaFile_CPP ${CMAKE_MOD_ELUNA_ENGINE_DIR}/LuaEngine/*.cpp )
 file(GLOB_RECURSE sources_ElunaFile_H ${CMAKE_MOD_ELUNA_ENGINE_DIR}/LuaEngine/*.h)
 
+file(GLOB_RECURSE sources_SOL_HPP ${CMAKE_MOD_ELUNA_ENGINE_DIR}/sol/*.hpp)
+
 set(ElunaLuaEngineFiles
   ${ElunaLuaEngineFiles}
   ${sources_ElunaFile_H}
   ${sources_ElunaFile_CPP}
+  ${sources_SOL_HPP}
 )
 
 source_group("LuaEngine\\Methods" FILES ${method_headers})
 source_group("LuaEngine\\Header Files" FILES ${sources_ElunaFile_H})
 source_group("LuaEngine\\Source Files" FILES ${sources_ElunaFile_CPP})
+source_group("LuaEngine\\sol2" FILES ${sources_SOL_HPP})
